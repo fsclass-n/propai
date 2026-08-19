1440bd28e69ede901b9d2a4473833080ba145a9c
wi <whyeil@naver.com>
2026-08-19 15:34:40 +0900

    first commit

---
+++ src/main/java/com/onrender/propai/PropaiApplication.java
@@ -0,0 +1,13 @@
+package com.onrender.propai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onrender/propai/PropaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onrender.propai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
